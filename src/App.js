29eee0b2094462ed99270963acae89bb5a4d5f51
mattcbodily <mattcbodily@gmail.com>
2020-05-12 08:44:46 -0600

    updated boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Hooks</h1>
+      {/* <h1>Value of input goes here</h1> */}
+      <div>
+        <input placeholder='Enter Your Name' />
+      </div>
+      {/* <h1>Count goes here</h1> */}
+      <div>
+        <button>Down</button>
+        <button>Up</button>
+      </div>
+      {/* ImageURL placed here */}
     </div>
   );
 }

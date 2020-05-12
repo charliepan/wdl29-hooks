5f96a2b759ccb45ba384b0296ca2223d7b0f608d
charliepan <hsinchih.pan@gmail.com>
2020-05-12 10:54:25 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(1),
+      [nameInput, setNameInput] = useState(''),
+      [imageURL, setImageURL] = useState('');
+
+  useEffect(()=>{
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${count}`)
+    .then(res => setImageURL(res.data.sprites.front_default))
+    .catch(err => console.log(err));
+  }, [count])
+
   return (
     <div className="App">
       <h1>React Hooks</h1>
-      {/* <h1>Value of input goes here</h1> */}
+      <h1>{nameInput}</h1>
       <div>
-        <input placeholder='Enter Your Name' />
+        <input placeholder='Enter Your Name' onChange={e =>setNameInput(e.target.value)} />
       </div>
-      {/* <h1>Count goes here</h1> */}
+      <h1>{count}</h1>
       <div>
-        <button>Down</button>
-        <button>Up</button>
+        <button onClick={()=> setCount(count - 1)}>Down</button>
+        <button onClick={()=> setCount(count + 1)}>Up</button>
       </div>
-      {/* ImageURL placed here */}
+      <img src={imageURL} alt="pokemon sprite"/>
     </div>
   );
 }
